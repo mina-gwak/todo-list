119e658c4af6a986a3d101b4ebd07c79d5c90ec7
mina-gwak <mina.gwak@kakao.com>
2022-04-11 12:10:33 +0900

    feat: Get column, task data and render them

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,12 +1,23 @@
 import '../style/style.scss';
 import ActionLayer from './components/ActionLayer.js';
-import ActionStore from './store/ActionStore.js'
+import ActionStore from './store/actionStore.js';
+import TaskStore from './store/taskStore.js';
+import ColumnStore from './store/columnStore.js';
 import Header from './components/Header.js';
+import Column from './components/Column.js';
 
 const init = async () => {
   await ActionStore.init();
+  await TaskStore.init();
+  await ColumnStore.init();
   new Header(document.querySelector('header'));
   new ActionLayer(document.querySelector('.action-layer'));
-}
+
+  const columnSections = document.querySelectorAll('.column');
+  const columns = ColumnStore.getAllColumns();
+  columnSections.forEach(
+    (section, index) => new Column(section, { column: columns[index] }),
+  );
+};
 
 init();
